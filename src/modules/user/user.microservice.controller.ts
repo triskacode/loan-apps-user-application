4feea4b86c9c505288be47df7c0a6fbc0d0756b8
Triska Mahfud K <triska.triskacode@gmail.com>
2022-11-07 12:52:26 +0000

    add endpoint find-by-id microservice

--- src/modules/user/user.microservice.controller.ts
+++ src/modules/user/user.microservice.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { FindUserByEmailDto } from './dto/find-user-by-email.dto';
+import { FindUserByIdDto } from './dto/find-user-by-id.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
@@ -11,5 +12,10 @@ export class UserMicroserviceController {
   @MessagePattern({ role: 'user', cmd: 'find-by-email-with-password' })
   async findByEmailWithPassword(dto: FindUserByEmailDto): Promise<User> {
     return this.userService.findByEmailWithPassword(dto.email);
+  }
+
+  @MessagePattern({ role: 'user', cmd: 'find-by-id' })
+  async findById(dto: FindUserByIdDto): Promise<User> {
+    return this.userService.findById(dto.id);
   }
 }
